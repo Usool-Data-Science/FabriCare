9983a4df61fc43abf4e594df5e05475d521319ba
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2025-01-24 04:53:14 +0100

    Create dedicated render.sh entry file

---
+++ backend/render.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+pip install -r requirements.txt
+pip install gunicorn
+if [ -d "migrations" ]; then
+    echo "Migrations folder found. Running flask db upgrade head..."
+    while true; do
+        flask db upgrade head
+        # flask fakes create 5    # No need to populate again.
+        if [[ "$?" == "0" ]]; then
+            echo "Upgrade and fakes creation successful."
+            break
+        fi
+        echo "Upgrade command failed, retrying in 5 seconds..."
+        sleep 5
+    done
+else
+    echo "Migrations folder not found. Creating a new migration..."
+    flask db init
+    flask db migrate -m "Instantiate db"
+    while true; do
+        flask db upgrade
+        flask fakes create 5
+        if [[ "$?" == "0" ]]; then
+            echo "Upgrade and fakes creation successful."
+            break
+        fi
+        echo "Upgrade command failed, retrying in 5 seconds..."
+        sleep 5
+    done
+fi
